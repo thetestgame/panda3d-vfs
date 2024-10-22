8caedae39c36e5ae348f241e5ecee9fc4bc0a9f3
thetestgame <nxtlabs13@gmail.com>
2024-10-21 19:31:57 -0500

    Initial Source Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+long_description = """
+The panda3d-vfs module provides easy access to the virtual file system capabilities of Panda3D
+"""
+
+setup(
+    name='panda3d_vfs',
+    description='Panda3D module provides easy access to the virtual file system capabilities of Panda3D',
+    long_description=long_description,
+    license='MIT',
+    version='1.1.0',
+    author='Jordan Maxwell',
+    maintainer='Jordan Maxwell',
+    url='https://github.com/NxtStudios/panda3d-vfs',
+    packages=['panda3d_vfs'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ])
