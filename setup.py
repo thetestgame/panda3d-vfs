5d5bf2de830c97bc732ba12df14fd93b9bd78fc4
thetestgame <nxtlabs13@gmail.com>
2024-10-21 19:33:38 -0500

    Fix initial version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Panda3D module provides easy access to the virtual file system capabilities of Panda3D',
     long_description=long_description,
     license='MIT',
-    version='1.1.0',
+    version='1.0.0',
     author='Jordan Maxwell',
     maintainer='Jordan Maxwell',
     url='https://github.com/NxtStudios/panda3d-vfs',
